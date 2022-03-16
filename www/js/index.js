af7d2688d9e3e4eb657083fdfb72273f71456db0
PolTellJove <poltelljove@gmail.com>
2022-03-16 15:53:06 +0100

    Second commit

--- www/js/index.js
+++ www/js/index.js
@@ -1,29 +1,24 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
-// Wait for the deviceready event before using any of Cordova's device APIs.
-// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
 document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
-
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
+    let log = false;
+    $('#loginButton').click(function(){
+    	console.log($('#user').val()+"/"+$('#pass').val())
+    	$.ajax({
+    		method: "GET",
+    		url: $('#URL').val()+"/api/login/"+$('#user').val()+"/"+$('#pass').val(),
+    		//data : {"username":$('#user').val(),"password":$('#pass').val()},
+    		dataType: "json",
+    	}).done(function (info) {
+    		if (info["status"] == "OK"){
+    			localStorage.setItem("URL", $('#URL').val())
+    			localStorage.setItem("session_token", info["session_token"]);
+    			window.location.assign('test.html');
+    		} else if (info["status"] != "OK"){
+    			console.log(info["message"])
+    		}
+    	}).fail(function(){
+    		alert("URL no valida");
+    	});
+    });
 }
