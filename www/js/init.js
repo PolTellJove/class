1873be87820cc293022a09cdf9d6b147cdab31e1
PolTellJove <poltelljove@gmail.com>
2022-03-29 01:05:19 +0200

    adaptacio progressa adequadament

--- www/js/init.js
+++ www/js/init.js
@@ -27,25 +27,25 @@
                   //Vaciando el div de la pagina 2
                   $('#test-swipe-2').empty();
                   //Creacion de objetos graficos
-                  let newh1 = $("<h3>"+cursos["title"]+"</h3>");
+                  let newh1 = $("<h3 class='details_head'>"+cursos["title"]+"</h3>");
                   $('#test-swipe-2').append(newh1);
-                  let newh2 = $("<h5>Elements</h5>");
+                  let newh2 = $("<h5 class='details_titles'>Elements</h5>");
                   $('#test-swipe-2').append(newh2);
                   //for (let item in arrayCursos) {
                   for (let item in info["course"]["elements"]) {
-                    let newsummary = $("<p>"+info["course"]["elements"][item]["title"]+"</p>");
+                    let newsummary = $("<p class='details_elements'>"+info["course"]["elements"][item]["title"]+"</p>");
                     $('#test-swipe-2').append(newsummary);
                   }
-                  let newh3 = $("<h5>VR Tasks</h5>");
+                  let newh3 = $("<h5 class='details_titles'>VR Tasks</h5>");
                   $('#test-swipe-2').append(newh3);
                   for (let item in info["course"]["vr_tasks"]) {
-                    let newsummary2 = $("<p>"+info["course"]["vr_tasks"][item]["title"]+"</p>");
+                    let newsummary2 = $("<p class='details_elements'>"+info["course"]["vr_tasks"][item]["title"]+"</p>");
                     $('#test-swipe-2').append(newsummary2);
                   }
-                  let newh4 = $("<h5>No-VR Tasks</h5>");
+                  let newh4 = $("<h5 class='details_titles'>No-VR Tasks</h5>");
                   $('#test-swipe-2').append(newh4)
                   for (let item in info["course"]["tasks"]) {
-                    let newsummary3 = $("<p>"+info["course"]["tasks"][item]["title"]+"</p>");
+                    let newsummary3 = $("<p class='details_elements'>"+info["course"]["tasks"][item]["title"]+"</p>");
                     $('#test-swipe-2').append(newsummary3);
                   }  
                   //}
